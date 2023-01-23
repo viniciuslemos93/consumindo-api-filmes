29a529e911fda88d975afe8be9a7d8ee09e79536
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-22 23:47:55 -0300

    Criada a função para pegar o ID do filme - e criada a página de detalhes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -8,13 +8,48 @@ function pesquisarFilmes(e) {
 function buscarFilmes(filmePesquisa) {
     apiKey = '5c80a2a1'
     url = 'http://www.omdbapi.com/?apikey='
-    axios.get(`${url}${apiKey}&s=${filmePesquisa}`)
-    //axios.get('http://www.omdbapi.com/?s=' + filmePesquisa)
+    axios.get(`${url}${apiKey}&s=${filmePesquisa}`)    
   .then(function (response) {
     console.log(response);
-    
+    var filmes = response.data.Search;
+    var mostrarFilmes = '';
+
+    for (var i = 0; i < filmes.length; i++) {
+        mostrarFilmes += `
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="${filmes[i].Poster}" class="img-thumbnail">
+                    <h4>${filmes[i].Title}</h4>
+                    <p><a href="#"class="btn btn-primary" role="button" onclick="filmeDetalhes('${filmes[i].imdbID}')">Ver Detalhes</a></p>
+                </div>
+            </div>
+        `;
+    }
+    document.getElementById('filmes').innerHTML = mostrarFilmes;
   })
   .catch(function (error) {
     console.log(error);
   });
+}
+
+function filmeDetalhes(id) {
+  sessionStorage.setItem('filmeId', id);
+  window.location = 'detalhes.html'
+  return false;
+}
+
+function mostraFilme() {
+  var filmeId = sessionStorage.getItem('filmeId');
+
+  apiKey = '5c80a2a1'
+  url = 'http://www.omdbapi.com/?apikey='
+    axios.get(`${url}${apiKey}&i=${filmeId}`)    
+  .then(function (response) {
+    var filme = response.data
+    console.log(filme);
+    
+  })
+    .catch(function (error) {
+      console.log(error);
+    });
 }
