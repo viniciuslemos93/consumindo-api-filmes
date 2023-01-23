bc3b6284ac6f8542aff7391110eff3bdbf79c772
Vinicius Lemos <vinycius1993@gmail.com>
2023-01-22 21:53:40 -0300

    realizada a comunicação com a API

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,20 @@
 document.getElementById('formulario').addEventListener('click', pesquisarFilmes);
 
 function pesquisarFilmes(e) {
-    
+    var filmePesquisa = document.getElementById('pesquisar').value
+    buscarFilmes(filmePesquisa)
     e.preventDefault();
+}
+function buscarFilmes(filmePesquisa) {
+    apiKey = '5c80a2a1'
+    url = 'http://www.omdbapi.com/?apikey='
+    axios.get(`${url}${apiKey}&s=${filmePesquisa}`)
+    //axios.get('http://www.omdbapi.com/?s=' + filmePesquisa)
+  .then(function (response) {
+    console.log(response);
+    
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 }
